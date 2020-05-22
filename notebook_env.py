9f877668c444a980974e47978b84f58096bb13fb
Alejandro F Queiruga <afq@google.com>
2020-05-22 09:12:19 +0000

    experiments and analysis

---
+++ notebook_env.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+import functools
+from glob import glob
+import json
+import os
+import re
+from typing import List
+
+from matplotlib import pylab as plt
+import numpy as np
+import pandas as pd
+import torch
+from SimDataDB import SimDataDB
+try:
+    import tqdm
+    progress = tqdm.tqdm
+except ModuleNotFoundError:
+    progress = lambda x, *args : x
+
+from odenet import refine_train
+from odenet import ode_models
+from odenet import datasets
+from odenet import odenet_cifar10
+from odenet import plotting
+from odenet import helper
+
+legend_name = lambda fname : re.search(r"ARCH(.*)", fname)[1]
+
+class PostPack:
+    """We should've saved Pack instead."""
+    scheme: str
+    use_skip_init: bool
+    use_batch_norms: str
+    initial_time_d: int
+    n_steps_per: int
+    refine: List[int]
+    def __init__(self, fname):
+        r = re.search(r"""ARCH-16-([A-Za-z]*)-([A-Za-z]*)-([a-z4_]*)-([0-9]*)-1.0-([0-9]*)-.*-160-(None|\[.*\])""",fname)
+        self.use_batch_norms = r.group(1)
+        self.use_skip_init = (r.group(2)!="NoSkip")
+        self.scheme = r.group(3)
+        self.initial_time_d = int(r.group(4))
+        self.n_steps_per = int(r.group(5))
+        if r.group(6) == "None":
+            self.refine = []
+        else:
+            self.refine = json.loads(r.group(6))
+        self.final_time_d = self.initial_time_d * 2**len(self.refine)
+
+def set_ode_config(model, n_steps, scheme, use_adjoint=False):
+    for net_idx in range(len(model.net)):
+        try:
+            model.net[net_idx].set_n_time_steps(n_steps)
+            model.net[net_idx].scheme = scheme
+        except AttributeError:
+            pass
--- script_it.py
+++ script_it.py
@@ -12,8 +12,8 @@ class Pack:
     initial_time_d: int  # t in [0,1] cut up into time_d slices
         # t=1, t=2, t=3,  -> if resnet epsilon = 1, then
         # t=1/3, t=2/3, t=1.0 -> resnet epsilon = 1/3
-    time_epsilon: float  # xdot = epsilon * f(x,t)
-    use_batch_norms: bool
+    time_epsilon: float = 1.0  # xdot = epsilon * f(x,t)
+    use_batch_norms: str = ""
     use_skip_init: bool = False
     
     n_time_steps_per: int = 1
@@ -33,34 +33,186 @@ class Pack:
     shape_function: str = 'piecewise'
     
 cuda0_batch = [
-    #(2,[40,80]),
+    (2,[40,80]),
     (2,[50,100]),
     (2,[60,120]),
 ]
-cuda1_batch = [
-    (1,[10,40,60,80])
-]
-args_list = [
+nothing = [
     Pack("CIFAR10", "SingleSegment", scheme, alpha,
          initial_time_d, epsilon, False,
          refine=refine,
          refine_variance=refine_variance,
          use_skip_init=True,
          n_time_steps_per=n_time_steps_per,
-         lr=0.05,
+         lr=0.1,
          lr_decay=0.1,
          use_adjoint=use_adjoint,
          lr_update=[80, 120, 140]
         )
     for n_time_steps_per in [1]
     for epsilon in [1.0]
-    for initial_time_d, refine in cuda1_batch
+    for initial_time_d, refine in []
     for refine_variance in [0.0]
     for alpha in [16]
     for scheme in [ "rk4_classic", ]
     for use_adjoint in [ False ]
 ]
+reference_test = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, time_d,
+         use_skip_init=False,
+         use_batch_norms="ode",
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140])
+    for time_d in [1, 2, 3, 4]
+    for scheme in ["rk4_classic"]
+]
 
+# Experiment at 12:49 May 17
+# Check refinement configurations of Euler vs rk4
+cuda1_batch = ["euler"]
+cuda2_batch = ["rk4_classic"]
+refinements_batch = [
+    (1,[10,20,30]),
+    (1,[20,30,40]),
+    (1,[30,45,60]),
+    (2,[30,40]),
+    (2,[45,60]),
+    (2,[60,120]),
+    (2,[50,100]),
+    (2,[40,80]),
+    (2,[30,60]),
+    (1,[10,20,40,60]),
+    (1,[10,20,30,40,50]),
+]
+experiment_1249May17_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=False,
+         use_batch_norms="ode",
+         refine=refine,
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140])
+    for initial_time_d, refine in refinements_batch
+    for scheme in cuda2_batch
+]
+
+# Experiment at 1:22 May 17
+# Try adjoint with different n_time_steps_per
+# Added isnan for this
+refinements_batch = [
+    (4,[]),
+    (8,[]),
+    (1,[20,40,]),
+    (1,[20,40,60,])
+]
+n_time_steps_per_batch = [ 1, 2, 3, 4 ]
+experiment_122May17_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=False,
+         use_batch_norms="ode",
+         refine=refine,
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140],
+         use_adjoint=True)
+    for initial_time_d, refine in refinements_batch
+    for scheme in ["euler", "rk4_classic",]
+]
+
+# Experiment at 1:22 May 18
+# Try adjoint with different n_time_steps_per
+# Previous experiment didn't do that >.<
+# Added isnan for this
+refinements_batch = [
+    (4,[]),
+    (8,[]),
+]
+n_time_steps_per_batch = [ 2, 3, 4 ]
+experiment_122May18_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=False,
+         use_batch_norms="ode",
+         refine=refine,
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140],
+         n_time_steps_per=n_time_steps_per,
+         use_adjoint=True)
+    for initial_time_d, refine in refinements_batch
+    for n_time_steps_per in n_time_steps_per_batch
+    for scheme in ["euler", "rk4_classic",]
+]
+
+# Experiment at 1:16 May 19
+# Doing no-refinement manifestation studies with SkipInit no batch norm
+cuda1_batch = ["euler"]
+cuda2_batch = ["rk4_classic"]
+experiment_116May19_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=True,
+         use_batch_norms=False,
+         refine=[],
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140],
+         n_time_steps_per=1,
+         use_adjoint=False)
+    for initial_time_d in [ 2, 4, 6, 8, 12 ]
+    for scheme in cuda2_batch
+]
+
+# Experiment at 18:53 May 19
+# Refinement with SkipInit and no BatchNorm for Euler/rk4
+refinements_batch = [
+    (2,[40, 60]),
+    (2,[40, 100]),
+    (2,[90, 130]),
+]
+cuda2_batch = ["euler"]
+cuda3_batch = ["rk4_classic"]
+experiment_1853May19_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=True,
+         use_batch_norms=False,
+         refine=refine,
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140],
+         n_time_steps_per=1,
+         use_adjoint=False)
+    for initial_time_d, refine in refinements_batch
+    for scheme in cuda3_batch
+]
+
+# Experiment at 3:30 May 19
+# Try adjoint with different n_time_steps_per
+# SkipInit
+refinements_batch = [
+    (4,[]),
+    (8,[]),
+]
+cuda0_batch = ["euler"]
+cuda2_batch = ["rk4_classic"]
+n_time_steps_per_batch = [ 1, 2, 3, 4 ]
+experiment_330May19_list = [
+    Pack("CIFAR10", "SingleSegment", scheme, 16, initial_time_d,
+         use_skip_init=True,
+         use_batch_norms=False,
+         refine=refine,
+         lr=0.1,
+         lr_decay=0.1,
+         lr_update=[80, 120, 140],
+         n_time_steps_per=n_time_steps_per,
+         use_adjoint=True)
+    for initial_time_d, refine in refinements_batch
+    for n_time_steps_per in n_time_steps_per_batch
+    for scheme in cuda0_batch
+]
+
+
+DEVICE = "cuda:0"
+args_list = experiment_330May19_list
 for args in args_list:
     driver.do_a_train_set(
         args.dataset,
@@ -82,4 +234,4 @@ for args in args_list:
         use_skip_init=args.use_skip_init,
         refine_variance=args.refine_variance,
         shape_function=args.shape_function,
-        device="cuda:1")
+        device=DEVICE)
