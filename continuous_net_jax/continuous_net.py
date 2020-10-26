1836f39bb462dd6f4e9bf014cb850be2a1cfc12c
Alejandro Francisco Queiruga <afq@google.com>
2020-10-26 00:39:09 -0700

    rest of the helpers

--- continuous_net_jax/continuous_net.py
+++ continuous_net_jax/continuous_net.py
@@ -6,7 +6,24 @@ import flax
 from nonauto_ode_solver import Euler
 from residual_modules import ShallowNet
 
-def SimpleContinuousNet(params, x, ode_dim, h_dim, o_dim, n_step=10, scheme=Euler):
+
+# Initialize tools
+def copy_and_perturb(params, n_basis):
+    """Make copies from a residual trace to make one per coefficient."""
+    prng_key = jax.random.PRNGKey(0)
+    key, *subkeys = jax.random.split(prng_key, 1+n_basis)
+    def _map(x, key):
+        return jnp.array(x) + 0.1*jax.random.normal(key, shape=x.shape)
+    return [jax.tree_map(lambda x : _map(x, k), params) for k in subkeys ]
+
+def init_ode_by_shape(prng_key, ode_model, n_basis, shape, *args):
+    """Make multiple coefficients from params using a flax model."""
+    _, params = ode_model.init_by_shape(
+        prng_key, shape, *args)
+    return copy_and_perturb(params, n_basis)
+
+
+def SimpleContinuousNet(params, x, ode_dim, h_dim, out_dim, n_step=10, scheme=Euler):
     """Toy version of a continuous net.
 
     This function is agnostic to the number of basis functions: it is 
@@ -15,7 +32,7 @@ def SimpleContinuousNet(params, x, ode_dim, h_dim, o_dim, n_step=10, scheme=Eule
     h = flax.nn.Dense.call(params['front'], x, features=ode_dim, bias=False)
     # Create the two functions:
     params_of_t = params_of_t_(params['ode'])  # theta(t)
-    f = jax.partial(ShallowNet.call, h_dim=h_dim, o_dim=ode_dim)  # f(theta, x)
+    f = jax.partial(ShallowNet.call, h_dim=h_dim, out_dim=ode_dim)  # f(theta, x)
     # Now loop over depth-time. This happens **statically**
     dt = 1.0/n_step
     for t in onp.linspace(0, 1, n_step):
@@ -24,5 +41,17 @@ def SimpleContinuousNet(params, x, ode_dim, h_dim, o_dim, n_step=10, scheme=Eule
         # But the general famility is this: (note how params_of_t is a function)
         h = scheme(params_of_t, h, t, f, dt)
     # Now do a linear classifier on the features of h(t=1)
-    y = flax.nn.Dense.call(params['back'], h, features=o_dim, bias=False)
+    y = flax.nn.Dense.call(params['back'], h, features=out_dim, bias=False)
     return jax.nn.sigmoid(y)
+
+def init_SimpleContinuousNet(ode_dim=3, h_dim=1, n_step=9, n_basis=3, scheme=RK4):
+    prng_key = jax.random.PRNGKey(0)
+    key, *subkeys = jax.random.split(prng_key, 3)
+    params = {
+        'front': flax.nn.Dense.init_by_shape(
+            prng_key, [(2,)], features=ode_dim, bias=False)[1],
+        'ode': init_ode_by_shape(
+            prng_key, ShallowNet, n_basis, [(ode_dim,)], h_dim, ode_dim),
+        'back':  flax.nn.Dense.init_by_shape(
+            prng_key, [(ode_dim,)], features=1, bias=False)[1],
+    }
