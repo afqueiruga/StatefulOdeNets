b09c761cb872beae4ff06b4a0d3314f92a19f875
Alejandro Francisco Queiruga <afq@google.com>
2020-11-26 22:09:33 -0800

    make datatransform handle multiple epochs

--- continuous_net_jax/data_transform.py
+++ continuous_net_jax/data_transform.py
@@ -1,7 +1,15 @@
+from typing import Any, Iterable, Tuple
+
 import jax.numpy as jnp
 
 
-def data_transform(dataset):
+class DataTransform:
     """Reorders torch formatted images to jax shapes and types."""
-    for X, Y in dataset:
-        yield jnp.array(X).transpose((0, 2, 3, 1)), jnp.array(Y)
+    dataset: Iterable[Tuple[Any, Any]]
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __iter__(self):
+        for X, Y in self.dataset:
+            yield jnp.array(X).transpose((0, 2, 3, 1)), jnp.array(Y)
--- continuous_net_jax/data_transform_test.py
+++ continuous_net_jax/data_transform_test.py
@@ -8,7 +8,7 @@ class DataTransformTest(unittest.TestCase):
 
     def test_data(self):
         data = [[jnp.ones((3, 1, 28, 28)), jnp.ones(3)]]
-        for X, Y in data_transform(data):
+        for X, Y in DataTransform(data):
             self.assertEqual(X.shape, (3, 28, 28, 1))
 
     def test_data_channels(self):
@@ -16,7 +16,18 @@ class DataTransformTest(unittest.TestCase):
             [jnp.ones((13, 8, 32, 32)), jnp.ones(3)],
             [jnp.ones((13, 8, 32, 32)), jnp.ones(3)],
         ]
-        for X, Y in data_transform(data):
+        for X, Y in DataTransform(data):
+            self.assertEqual(X.shape, (13, 32, 32, 8))
+
+    def test_multi_epoch(self):
+        data = [
+            [jnp.ones((13, 8, 32, 32)), jnp.ones(3)],
+            [jnp.ones((13, 8, 32, 32)), jnp.ones(3)],
+        ]
+        data_transform = DataTransform(data)
+        for X, Y in data_transform:
+            self.assertEqual(X.shape, (13, 32, 32, 8))
+        for X, Y in data_transform:
             self.assertEqual(X.shape, (13, 32, 32, 8))
 
 
