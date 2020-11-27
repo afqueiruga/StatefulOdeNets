7b233654925f5442c85a027eb7cc24ec8685b5f0
Alejandro Francisco Queiruga <afq@google.com>
2020-11-26 20:26:39 -0800

    need to convert types first

--- continuous_net_jax/data_transform.py
+++ continuous_net_jax/data_transform.py
@@ -4,4 +4,4 @@ import jax.numpy as jnp
 def data_transform(dataset):
     """Reorders torch formatted images to jax shapes and types."""
     for X, Y in dataset:
-        yield (jnp.array(X.transpose((0, 2, 3, 1))), jnp.array(Y))
+        yield jnp.array(X).transpose((0, 2, 3, 1)), jnp.array(Y)
