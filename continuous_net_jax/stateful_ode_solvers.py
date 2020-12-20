e86d31c0beb652abed08242b431a02c9c5646355
Alejandro Francisco Queiruga <afq@google.com>
2020-12-20 11:41:02 -0800

    Changed the stateful integration api

--- continuous_net_jax/stateful_ode_solvers.py
+++ continuous_net_jax/stateful_ode_solvers.py
@@ -30,8 +30,9 @@ def Euler(params_of_t: ContinuousParameters,
           f: RateEquation = None,
           Dt: float = 1.0) -> ArrayType:
     """Forward Euler, O(Dt)."""
-    state1, k1 = f(params_of_t(t0), x)
-    return state1, x + Dt * k1
+    t1 = t0
+    k1, state1 = f(params_of_t(t1), x)
+    return x + Dt * k1, ((t1, state1),)
 
 
 def Midpoint(params_of_t: ContinuousParameters,
@@ -40,10 +41,12 @@ def Midpoint(params_of_t: ContinuousParameters,
              f=None,
              Dt: float = 1.0) -> ArrayType:
     """Explicit Midpoint, a two stage Runge Kutta, O(Dt^2)."""
-    state1, k1 = f(params_of_t(t0), x)
-    x1 = x + 0.5 * Dt * k1  # t = 1/2
-    state2, k2 = f(params_of_t(t0 + 0.5 * Dt), x1)
-    return state2, x + Dt * k2
+    t1 = t0
+    k1, state1 = f(params_of_t(t0), x)
+    x1 = x + 0.5 * Dt * k1
+    t2 = t0 + 0.5 * Dt  # t = 1/2
+    k2, state2 = f(params_of_t(t2), x1)
+    return x + Dt * k2, ((t1, state1), (t2, state2))
 
 
 def RK4(params_of_t: ContinuousParameters,
@@ -52,33 +55,42 @@ def RK4(params_of_t: ContinuousParameters,
         f=None,
         Dt: float = 1.0):
     """The 'classic' RK4, a four stage Runge Kutta, O(Dt^4)."""
-    state1, k1 = f(params_of_t(t0), x)  # t = 0+ (inside of domain)
+    t1 = t0  # t = 0+ (inside of domain)
+    k1, state1 = f(params_of_t(t0), x)
     x1 = x + 0.5 * Dt * k1
-    state2, k2 = f(params_of_t(t0 + 0.5 * Dt), x1)  # t = 1/2
+    t2 = t0 + 0.5 * Dt  # t = 1/2
+    k2, state2 = f(params_of_t(t2), x1)
     x2 = x + 0.5 * Dt * k2
-    state3, k3 = f(params_of_t(t0 + 0.5 * Dt), x2)  # t = 1/2
+    t3 = t0 + 0.5 * Dt  # t = 1/2
+    k3, state3 = f(params_of_t(t3), x2)
     x3 = x + Dt * k3
-    state4, k4 = f(params_of_t(t0 + Dt - DT_OPEN_SET_EPS), x3)  # t = 1- (inside domain)
-    return x + Dt * (1.0 / 6.0 * k1 + 1.0 / 3.0 * k2 + 1.0 / 3.0 * k3 +
-                     1.0 / 6.0 * k4)
+    t4 = t0 + Dt - DT_OPEN_SET_EPS  # t = 1- (inside domain)
+    k4, state4 = f(params_of_t(t4), x3)
+    x_out = x + Dt * (1.0 / 6.0 * k1 + 1.0 / 3.0 * k2 + 1.0 / 3.0 * k3 +
+                      1.0 / 6.0 * k4)
+    return x_out, ((t1, state1), (t2, state2), (t3, state3), (t4, state4))
 
 
 def RK4_38(params_of_t: ContinuousParameters,
-        x: ArrayType,
-        t0: float = 0,
-        f=None,
-        Dt: float = 1.0):
+           x: ArrayType,
+           t0: float = 0,
+           f=None,
+           Dt: float = 1.0):
     """The 3/8s RK4, a four stage Runge Kutta, O(Dt^4)."""
-    state1, k1 = f(params_of_t(t0), x)  # t = 0+ (inside of domain)
-    x1 = x + 1.0/3.0 * Dt * k1
-    state2, k2 = f(params_of_t(t0 + 1.0/3.0 * Dt), x1)  # t = 1/3
-    x2 = x + Dt * (-1.0/3.0 * k1 +  1.0 * k2)
-    state3, k3 = f(params_of_t(t0 + 2.0/3.0 * Dt), x2)  # t = 2/2
-    x3 = x + Dt *( k1 - k2 + k3)
-    state4, k4 = f(params_of_t(t0 + Dt - DT_OPEN_SET_EPS), x3)  # t = 1-
-    xf = x + Dt * (1.0 / 8.0 * k1 + 3.0 / 8.0 * k2 + 3.0 / 8.0 * k3 +
-                     1.0 / 8.0 * k4)
-    return state4, xf
+    t1 = t0  # t = 0+ (inside of domain)
+    k1, state1 = f(params_of_t(t0), x)  # t = 0+ (inside of domain)
+    x1 = x + 1.0 / 3.0 * Dt * k1
+    t2 = t0 + 1.0 / 3.0 * Dt
+    k2, state2 = f(params_of_t(t2), x1)  # t = 1/3
+    x2 = x + Dt * (-1.0 / 3.0 * k1 + 1.0 * k2)
+    t3 = t0 + 2.0 / 3.0 * Dt
+    k3, state3 = f(params_of_t(), x2)  # t = 2/2
+    x3 = x + Dt * (k1 - k2 + k3)
+    t4 = t0 + Dt - DT_OPEN_SET_EPS
+    k4, state4 = f(params_of_t(t4), x3)  # t = 1-
+    x_out = x + Dt * (1.0 / 8.0 * k1 + 3.0 / 8.0 * k2 + 3.0 / 8.0 * k3 +
+                      1.0 / 8.0 * k4)
+    return x_out, ((t1, state1), (t2, state2), (t3, state3), (t4, state4))
 
 
 SCHEME_TABLE = {
@@ -90,21 +102,23 @@ SCHEME_TABLE = {
 
 
 def StateOdeIntegrateFast(params_of_t: ContinuousParameters,
-                             x: ArrayType,
-                             f: RateEquation,
-                             scheme: IntegrationScheme = Euler,
-                             n_step: int = 10) -> ArrayType:
+                          x: ArrayType,
+                          f: RateEquation,
+                          scheme: IntegrationScheme = Euler,
+                          n_step: int = 10) -> ArrayType:
     dt = 1.0 / n_step
+    states = []
     for t in onp.linspace(0, 1, n_step):
         state, x = scheme(params_of_t, x, t, f, dt)
-    return x
+        states.extend(state)
+    return states, x
 
 
 def StateOdeIntegrateWithPoints(params_of_t: ContinuousParameters,
-                           x: ArrayType,
-                           f: RateEquation,
-                           scheme: IntegrationScheme = Euler,
-                           n_step: int = 10) -> List[ArrayType]:
+                                x: ArrayType,
+                                f: RateEquation,
+                                scheme: IntegrationScheme = Euler,
+                                n_step: int = 10) -> List[ArrayType]:
     dt = 1.0 / n_step
     xs = [onp.array(x)]
     for t in onp.linspace(0, 1, n_step):
--- continuous_net_jax/stateful_ode_solvers_test.py
+++ continuous_net_jax/stateful_ode_solvers_test.py
@@ -2,16 +2,54 @@ import unittest
 
 from .stateful_ode_solvers import *
 
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+from jax.config import config
+config.enable_omnistaging()
+
 def stateful_f(p, x):
-    return p + 1.0, x + p + 1.0
+    return x + p + 1.0, p + 1.0
+
+
+class FlaxMod(nn.Module):
+    @nn.compact
+    def __call__(self, x):
+        h = nn.Dense(1)(x)
+        return nn.BatchNorm()(h)
+
 
 class StatefulIntegratorsTests(unittest.TestCase):
 
     def test_euler(self):
         params_of_t = lambda t: 2.0
-        state, x2 = Euler(params_of_t, 3.0, 0.0, stateful_f, 1.0)
-        self.assertEqual(state, 3.0)
-        self.assertEqual(x2, 9.0)
+        x_out, state = Euler(params_of_t, 3.0, 0.0, stateful_f, 1.0)
+        self.assertEqual(state, ((0.0, 3.0),))
+        self.assertEqual(x_out, 9.0)
+
+    def test_midpoint(self):
+        params_of_t = lambda t: 0.0
+        x_out, state = Midpoint(params_of_t, 1.0, 0.0, stateful_f, 1.0)
+
+        self.assertEqual(state, ((0.0, 1.0), (0.5, 1.0)))
+        self.assertEqual(x_out, 4.0)  # 1.0 + 1.0*( 1.0 + 0.5*(1.0+1.0) + 1.0)
+
+    def test_flax_module(self):
+        prng_key = jax.random.PRNGKey(0)
+        x = jnp.array([1.0])
+        n_basis = 2
+        params = FlaxMod().init(prng_key, x)
+        init_state, train_params = params.pop('params')
+        params_of_t = lambda t: params
+ 
+        f = lambda *args, **kwargs: FlaxMod().apply(*args, mutable=init_state.keys(), **kwargs)
+        x_out, state_out = Euler(params_of_t, x, t0=0.0, f=f, Dt=1.0)
+
+        # print(state)
+        # print(x_out)
+        self.assertEqual(state_out[0][0], 0.0)
+        self.assertEqual(state_out[0][1].keys(), init_state.keys())
+
 
 if __name__ == "__main__":
     unittest.main()
