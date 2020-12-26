c255812710100180e4595db2d379e57ffe28ac94
Alejandro Francisco Queiruga <afq@google.com>
2020-12-25 17:07:35 -0800

    point wise projection

--- continuous_net_jax/basis_functions.py
+++ continuous_net_jax/basis_functions.py
@@ -1,8 +1,12 @@
+import jax
+import jax.numpy as jnp
+
 from .continuous_types import *
+
 
 # Basis functions
 def piecewise_constant(param_nodes: Iterable[JaxTreeType], t: float,
-                         n_basis: int) -> JaxTreeType:
+                       n_basis: int) -> JaxTreeType:
     """A piecewise constant basis set."""
     idx = min(int(n_basis * t), n_basis - 1)
     return param_nodes[idx]
@@ -17,3 +21,25 @@ def params_of_t(
         return basis(param_nodes, t, len(param_nodes))
 
     return theta
+
+
+def hessian(f):
+    return jax.jacfwd(jax.jacrev(f))
+
+
+def point_loss(params, basis, ts, ys):
+    losses = []
+    for t_i, y_i in zip(ts, ys):
+        loss = (params_of_t(params, basis=basis)(t_i) - y_i)**2
+        losses.append(loss)
+    return jnp.sum(jnp.array(losses))
+
+
+def point_project(ys, ts, n_basis, basis):
+    params = jnp.zeros(n_basis)
+    vG = jax.grad(point_loss)(params, basis, ts, ys)
+    mH = hessian(point_loss)(params, basis, ts, ys)
+    # vG = jnp.stack(G)
+    # mH = jnp.stack([jnp.stack(row) for row in H])
+    d_params = jnp.linalg.solve(mH, vG)
+    return d_params
--- continuous_net_jax/basis_functions_test.py
+++ continuous_net_jax/basis_functions_test.py
@@ -2,15 +2,22 @@ import unittest
 
 from .basis_functions import *
 
+
 class BasisFunctionsTests(unittest.TestCase):
 
     def test_list_of_floats(self):
         nodes = [1.0, 3.0, 2.0]
         theta_t = params_of_t(nodes, piecewise_constant)
-        self.assetEqual(theta_t(0.0), 1.0)
-        self.assetEqual(theta_t(0.5), 3.0)
-        self.assetEqual(theta_t(1.000001), 2.0)
-        self.assetTrue(False)
+        self.assertEqual(theta_t(0.0), 1.0)
+        self.assertEqual(theta_t(0.5), 3.0)
+        self.assertEqual(theta_t(1.000001), 2.0)
+
+    def test_point_project_floats(self):
+        ts = list(jnp.linspace(0, 1, 11))
+        points = [jnp.sin(3 * t) for t in ts]
+        n = 13
+        nodes = point_project(points, ts, n, piecewise_constant)
+        self.assertEqual(len(nodes), n)
 
 
 if __name__ == "__main__":
