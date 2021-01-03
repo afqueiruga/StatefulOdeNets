99bf6ade675bd830b4ebe5e5bfa1c2c180d9a2fd
Alejandro Francisco Queiruga <afq@google.com>
2021-01-02 18:19:51 -0800

    Use jax's builtin hessian

--- continuous_net_jax/basis_functions.py
+++ continuous_net_jax/basis_functions.py
@@ -18,10 +18,6 @@ def piecewise_constant(param_nodes: Iterable[JaxTreeType]) -> ContinuousParamete
     return theta
 
 
-def hessian(f):
-    return jax.jacfwd(jax.jacrev(f))
-
-
 def point_loss(params, basis, ts, ys):
     losses = []
     for t_i, y_i in zip(ts, ys):
@@ -33,7 +29,7 @@ def point_loss(params, basis, ts, ys):
 def point_project(ys, ts, n_basis, basis):
     params = jnp.zeros(n_basis)
     vG = jax.grad(point_loss)(params, basis, ts, ys)
-    mH = hessian(point_loss)(params, basis, ts, ys)
+    mH = jax.hessian(point_loss)(params, basis, ts, ys)
     d_params = -jnp.linalg.solve(mH, vG)
     return d_params
 
