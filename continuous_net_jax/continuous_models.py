ef57070102167e10a1afd9df76f29c21e82450ec
erichson <erichson@uw.edu>
2021-05-18 08:06:04 -0600

    fine tuning

--- continuous_net_jax/continuous_models.py
+++ continuous_net_jax/continuous_models.py
@@ -71,8 +71,13 @@ class ContinuousImageClassifier(nn.Module):
                     kernel_size=(3, 3),
                     kernel_init=INITS[self.kernel_init])(x)
         h = NORMS[self.norm](use_running_average=not self.training)(h)        
-        h = nn.relu(h)
+        h = nn.gelu(h)
         # 3 stages of continuous segments:
+        h = ResidualStitch(hidden_features=self.hidden * self.alpha,
+                           output_features=self.hidden * self.alpha,
+                           strides=(1, 1),
+                           norm=self.norm,
+                           training=self.training)(h)        
         h = ContinuousNet(R=R_(self.hidden * self.alpha),
                           scheme=self.scheme,
                           n_step=self.n_step,
@@ -103,7 +108,7 @@ class ContinuousImageClassifier(nn.Module):
                           training=self.training)(h)
         # Pool and linearly classify:
         h = NORMS[self.norm](use_running_average=not self.training)(h)        
-        h = nn.relu(h)
+        h = nn.gelu(h)
         h = nn.avg_pool(h, window_shape=(8, 8), strides=(8, 8))
         h = h.reshape((h.shape[0], -1))
         h = nn.Dense(features=self.n_classes)(h)
